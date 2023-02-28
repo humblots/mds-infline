dbfe013bf49651eec9c5619c46c1ebfc85494ccb
Stephane Humblot <shumblot@oph74.fr>
2023-02-28 19:24:39 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import "./assets/css/tailwind.css";
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      integralCF: ["IntegralCF", "Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        purple: "#B188EA",
+        grey: "#D8D8D8",
+      },
+    },
+  },
+  plugins: [],
+};
